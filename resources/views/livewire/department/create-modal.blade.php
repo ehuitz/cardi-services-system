9a3dc46d88c6b4c80f36d7fdc293de4546b47824
Elmer Huitz <elmohuitz@gmail.com>
2022-03-18 15:41:11 -0600

    another update

--- resources/views/livewire/department/create-modal.blade.php
+++ resources/views/livewire/department/create-modal.blade.php
@@ -14,32 +14,14 @@
 			placeholder="Name"
 			wire:model.defer="name"
 		/>
-		{{-- <x-forms.model-dropdown
-			:identifier="'create_model'"
-			:label="'Model'"
-			:id="'create_model'"
-			:name="'create_model'"
-			wire:model.defer="deviceModel"
-		/> --}}
-		<x-forms.country-dropdown
+
+		<x-forms.countryall-dropdown
 			:identifier="'create_country'"
 			:label="'Country'"
 			:id="'create_country'"
 			:name="'create_country'"
 			wire:model.defer="country"
 		/>
-		{{-- <x-forms.input
-			label="Serial Number"
-			id="create_serial_number"
-			placeholder="Serial Number"
-			wire:model.defer="serialNumber"
-		/>
-		<x-forms.input
-			label="MAC Address"
-			id="create_mac_address"
-			placeholder="MAC Address"
-			wire:model.defer="macAddress"
-		/> --}}
 	</x-slot>
 
 	<x-slot name="buttonText">
